deb9661e3ca10561846f9c3552478e40fbf770dd
Alex Holachek <aholachek@gmail.com>
2017-04-10 23:07:00 -0400

    show absentee percentage

--- src/routes/legislator/ProgressBarWContextComponent.js
+++ src/routes/legislator/ProgressBarWContextComponent.js
@@ -24,7 +24,7 @@ export default class ProgressBarWContext extends React.Component {
           large={this.props.large}
         />
         <small style={{ lineHeight: 1.3, display: 'inline-block', marginTop : '.3rem' }}>
-          *Wasn't present for 10-50% of scored votes, either because of absences or mid-term election
+          {`*Wasn't present for ${100 - d.recordedVotePercentage}% of scored votes, either because of absences or mid-term election`}
         </small>
       </div>
       )
