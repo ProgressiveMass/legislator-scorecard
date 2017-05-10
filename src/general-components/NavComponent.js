8f9aaa023e40405d0c6b20f99514ac314317c397
Scott <scottaohara@gmail.com>
2017-05-09 23:06:38 -0400

    updates to header/nav comonent
    • add ‘opens in new window’ to necessary link
    • View All MA Legislators link: add <abbr> element to wrap ‘MA’, with aria-label to announce it as “massachusetts”
    • update progressive MA logo component by removing extra div wrappers, converting them to spans, and removing alt text that would have made the link overly verbouse.
    • update where the <nav> element starts/ends to include the logo as a home link within the navigation landmark.  add the aria-label=‘primary’ to announce the navigation as “primary navigation”

--- src/general-components/NavComponent.js
+++ src/general-components/NavComponent.js
@@ -3,32 +3,31 @@ import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const Header = (props) => {
   return (<header>
-    <div className='module-container py-3 d-md-flex justify-content-between align-items-center'>
+    <nav className='module-container py-3 d-md-flex justify-content-between align-items-center' aria-label='primary'>
+
       <Link to='/' className='header__home-link d-flex align-items-center'>
-        <div>
-          <img src={require('./../img/logo.svg')}
-            alt='logo image of an inspection sheet'
-            style={{ width: '35px', marginRight: '.7rem' }}
-          />
-        </div>
-        <div>
-          <div className='text-uppercase h6 mb-0' style={{ fontWeight: 'normal' }}>Progressive Massachusetts</div>
-          <div className='h4 mb-0'>Legislator Scorecard</div>
-        </div>
+        <img src={require('./../img/logo.svg')}
+          alt=''
+          style={{ width: '35px', marginRight: '.7rem' }} />
+
+        <span>
+          <span className='text-uppercase h6 mb-0 d-block' style={{ fontWeight: 'normal' }}>Progressive Massachusetts</span>
+          <span className='h4 mb-0 d-block'>Legislator Scorecard</span>
+        </span>
       </Link>
 
-      <nav className='text-md-right mt-3 mt-md-0'>
+      <div className='text-md-right mt-3 mt-md-0'>
         <ul className='list-unstyled d-flex'>
           <li className='mr-3 mr-md-5'>
-            <NavLink to='/all-legislators'>View All MA Legislators</NavLink>
+            <NavLink to='/all-legislators'>View All <abbr aria-label='Massachusetts'>MA</abbr> Legislators</NavLink>
           </li>
           <li>
-            <a href='https://gdoc.pub/doc/19eWMYZ3IZaT-YFqswn-LqGOnYzHMID7LXEj1Gn1GNu0' target='_blank'>F.A.Q.</a>
+            <a href='https://gdoc.pub/doc/19eWMYZ3IZaT-YFqswn-LqGOnYzHMID7LXEj1Gn1GNu0' target='_blank'>F.A.Q. <span className='sr-only'>opens in new window</span></a>
           </li>
         </ul>
 
-      </nav>
-    </div>
+      </div>
+    </nav>
   </header>)
 }
 
