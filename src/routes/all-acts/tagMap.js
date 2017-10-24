19ee9a793823808efee9589ec7bfd318c2d23a13
Jay Spiros <jspiros@gmail.com>
2017-10-24 08:04:08 -0400

    swapped money logo for group logo in shared prosperity

--- src/routes/all-acts/tagMap.js
+++ src/routes/all-acts/tagMap.js
@@ -1,6 +1,6 @@
 
 export default {
-  'shared prosperity': { name: 'economy', badge: 'badge-warning', logo: 'money.svg' },
+  'shared prosperity': { name: 'economy', badge: 'badge-warning', logo: 'group.svg' },
   'all means all': { name:'justice & equality', badge: 'badge-purple', logo: 'balance.svg' },
   'good govt/strong democracy': { name: 'government', badge: 'badge-info', logo: 'gvmnt.svg' },
   'infrastructure/environment': { name: 'environment', badge: 'badge-success', logo: 'globe.svg' }
