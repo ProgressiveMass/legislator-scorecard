d35589435aec8420b597fc62e0c8c9d45fb77430
Jay Spiros <jspiros@gmail.com>
2018-10-01 11:18:38 -0400

    first pass adding fall 2017 votes

--- src/routes/all-acts/tagMap.js
+++ src/routes/all-acts/tagMap.js
@@ -4,6 +4,7 @@ export default {
   'all means all': { name:'justice & equality', badge: 'badge-purple', logo: 'balance.svg' },
   'good govt/strong democracy': { name: 'government', badge: 'badge-info', logo: 'gvmnt.svg' },
   'good government & strong democracy': { name: 'government', badge: 'badge-info', logo: 'gvmnt.svg' },
+  'strong democracy': { name: 'government', badge: 'badge-info', logo: 'gvmnt.svg' },
   'infrastructure/environment': { name: 'environment', badge: 'badge-success', logo: 'globe.svg' },
   'sustainable infrastructure & environmental protection': { name: 'environment', badge: 'badge-success', logo: 'globe.svg' }
 }
--- src/routes/legislator/tagMap.js
+++ src/routes/legislator/tagMap.js
@@ -4,6 +4,7 @@ export default {
   'all means all' : { name :'justice & equality', badge : 'badge-purple' },
   'good govt/strong democracy' : { name : 'government', badge : 'badge-info' },
   'good government & strong democracy': { name: 'government', badge: 'badge-info' },
+  'strong democracy': { name: 'government', badge: 'badge-info' },
   'infrastructure/environment' : { name : 'environment', badge : 'badge-success' },
   'sustainable infrastructure & environmental protection': { name: 'environment', badge: 'badge-success' }
 }
