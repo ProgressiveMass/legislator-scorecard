5e2fc0aa93c09dbc2c20aab4a1a2ae0530eade04
Dan Cusher <dscush@gmail.com>
2021-08-14 21:09:56 -0400

    Add script to download all social photos for more efficient manual review

---
+++ test/downloadSocialImages.js
@@ -0,0 +1,33 @@
+var env;
+if (process.env.STAGING) {
+  env = 'staging';
+} else if (process.env.PRODUCTION){
+  env = 'production';
+} else {
+  env = 'development';
+}
+const result = require("dotenv").config({
+  path: '.env.' + env,
+})
+const request = require('request')
+const fs = require('fs')
+if (result.error) {
+  throw result.error
+}
+
+const houseLegislators = require('../src/data/house_legislators.json')
+const senateLegislators = require('../src/data/senate_legislators.json')
+const domain = process.env.GATSBY_DOMAIN;
+
+houseLegislators.concat(senateLegislators).forEach(legislator => {
+
+  const ogImageFilename =
+    (legislator.name + '-' + legislator.district)
+    .toLowerCase()
+    .replaceAll(' ', '-')
+    .replaceAll(/[.,']/g, '')
+    .normalize("NFD").replace(/[\u0300-\u036f]/g, "");  // stackoverflow.com/questions/990904
+  const url = `${domain}og-images/legislator/${ogImageFilename}.png`;
+  request(url).pipe(fs.createWriteStream('test/tmp/' + ogImageFilename));
+});
+
