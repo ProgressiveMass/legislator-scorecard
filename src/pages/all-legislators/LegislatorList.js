ae200baf8ed95088f69b69b9da7ad5b31546d1f0
Dan Cusher <dscush@gmail.com>
2026-03-31 09:10:48 -0400

    Fix session years in LegislatorList

--- src/pages/all-legislators/LegislatorList.js
+++ src/pages/all-legislators/LegislatorList.js
@@ -88,7 +88,7 @@ const LegislatorRow = ({ legislator, i, chamber, sessionNumber }) => {
       <td data-label='Party'>
         <span>{legislator.party.slice(0, 1)}</span>
       </td>
-      <td data-label='Progressive Rating (2024-2025)'>
+      <td data-label='Progressive Rating (2025-2026)'>
         <div style={{ maxWidth: '300px' }}>
           <ProgressBar data={legislator} sessionNumber={sessionNumber} />
         </div>
@@ -213,7 +213,7 @@ const LegislatorList = (props) => {
                   onClick={handleSort}
                   sort='score'
                   currentSort={sort}
-                  title='Prog. Rating (2024-2025)'
+                  title='Prog. Rating (2025-2026)'
                 />
               </th>
             </tr>
